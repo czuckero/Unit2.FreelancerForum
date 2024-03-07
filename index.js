049b7ee72d5ad0480541a9c54d69ddb4a6d8bc46
Christian Zuckero <christianazuckero@gmail.com>
2024-03-07 01:35:50 -0500

    Created function to generate random freelancer objects

---
+++ index.js
@@ -0,0 +1,46 @@
+// Here are names, occupations, and prices of potential freelancers
+const names = ["Dr. Oblong", "Uncle Barry", "Sir Mountaintop", "Dr. Raphael", "Professor Overlord Williams"];
+const occupations = ["Teacher", "Programmer", "Gardener", "Writer", "Driver", "Gamer"];
+
+// Array of initial freelancers
+const freelancers = [
+    { name: "Marty Fletcher", occupation: "Writer", startingPrice: 25 },
+    { name: "Olin Feebstocking", occupation: "Programmer", startingPrice: 50 },
+    { name: "Dr. Dunkmeister", occupation: "Teacher", startingPrice: 45 },
+  ];
+  
+// Function to render the intial freelancers into the table
+  renderInitialFreelancers();
+
+  function renderInitialFreelancers() {
+    // Selecting the freelancers id in the table
+    const initialFreelancers = document.querySelector("#freelancers");
+    // Iterating through the freelancers object array to display each freelancer property in the table
+    const initialFreelancerElements = freelancers.map((freelancer) => {
+        const element = document.createElement("tr");
+        console.log(element);
+        element.innerHTML = `<td>${freelancer.name}</td> <td>${freelancer.occupation}</td> <td>${freelancer.startingPrice}</td>`;
+        return element;
+    });
+    initialFreelancers.replaceChildren(...initialFreelancerElements);
+
+    const avg = document.querySelector("#avg"); 
+    
+}
+
+function generateRandomFreelancer() {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    const randomOccupation = occupations[Math.floor(Math.random() * occupations.length)];
+    const randomPrice = Math.floor(Math.random() * 100) + 1;
+    console.log(randomPrice);
+    freelancers.push({name: randomName, occupation: randomOccupation, startingPrice: randomPrice});
+    
+    renderInitialFreelancers();
+
+}
+
+function averagePrice() {
+
+}
+
+setInterval(generateRandomFreelancer, 5000);
